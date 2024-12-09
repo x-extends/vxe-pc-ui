c52d0ef3377b181986625274c020da83bf70d2e0
xuliangzhan <xlz26296x@163.com>
2024-12-09 08:00:55 +0800

    releases 4.3.22

--- types/components/table.d.ts
+++ types/components/table.d.ts
@@ -2711,6 +2711,8 @@ export interface TableReactData<D = any> {
   },
   scrollVMLoading: boolean
 
+  isCustomStatus: boolean
+
   isDragRowMove: Boolean
   dragRow: any
   isDragColMove: boolean
@@ -3041,6 +3043,10 @@ export interface TableMethods<DT = any> {
     tableColumn: VxeTableDefines.ColumnInfo<DT>[]
   }
   /**
+   * 获取表格的全量列
+   */
+  getFullColumns(): VxeTableDefines.ColumnInfo[]
+  /**
    * 获取数据，和 data 的行为一致，也可以指定索引获取数据
    */
   getData(): DT[]
@@ -3082,6 +3088,10 @@ export interface TableMethods<DT = any> {
     tableData: DT[]
     footerData: DT[][]
   }
+  /**
+   * 获取表格的全量数据，如果是 tree-config 则返回带层级的树结构
+   */
+  getFullData(): DT[]
   /**
    * 设置指定列为固定列
    * @param fieldOrColumns 列对象或字段名,支持传多个
@@ -4407,6 +4417,7 @@ export namespace VxeTableDefines {
     columns: VxeTableDefines.ColumnInfo[]
     isAllChecked: boolean
     isAllIndeterminate: boolean
+    isCustomStatus: boolean
   }
 
   export interface RowDragSlotParams<D = any> {
