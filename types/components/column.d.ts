002c3807c5522e4cf3561f590967fa3b92c94458
xuliangzhan <xu_liangzhan@163.com>
2025-06-24 10:30:36 +0800

    releases 3.6.33

--- types/components/column.d.ts
+++ types/components/column.d.ts
@@ -158,7 +158,7 @@ export namespace VxeColumnPropTypes {
   /**
    * 指定聚合函数
    */
-  export type AggFunc = boolean | 'sum' | 'max' | 'count' | 'avg' | 'min' | 'max' | 'first' | 'last' | '' | null | undefined
+  export type AggFunc = boolean | 'sum' | 'count' | 'avg' | 'min' | 'max' | 'first' | 'last' | '' | null | undefined
 
   interface ExportMethodParams<D = any> {
     $table: VxeTableConstructor<D>
