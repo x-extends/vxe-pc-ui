57ca2a0906abd5d83a14114010c3d48a29b06f0c
xuliangzhan <xu_liangzhan@163.com>
2025-08-04 09:59:15 +0800

    releases 3.8.5

--- packages/table-select/src/table-select.ts
+++ packages/table-select/src/table-select.ts
@@ -294,20 +294,18 @@ export default defineVxeComponent({
       })
       if (multiple) {
         if (!hasCheckboxCol) {
-          errLog('vxe.error.reqProp', ['{ type: "checkbox" }'])
+          tableCols.unshift({
+            type: 'checkbox',
+            width: 70
+          })
         }
-        tableCols.unshift({
-          type: 'checkbox',
-          width: 70
-        })
       } else {
         if (!hasRadioCol) {
-          errLog('vxe.error.reqProp', ['{ type: "radio" }'])
+          tableCols.unshift({
+            type: 'radio',
+            width: 70
+          })
         }
-        tableCols.unshift({
-          type: 'radio',
-          width: 70
-        })
       }
       reactData.tableColumns = tableCols
     }
