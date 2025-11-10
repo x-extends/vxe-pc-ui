d612015aa4802526fd4b2363e8aad0ff8bba7817
xuliangzhan <xu_liangzhan@163.com>
2025-11-10 14:47:43 +0800

    releases 4.10.21

--- types/components/table-module/menu.d.ts
+++ types/components/table-module/menu.d.ts
@@ -1,3 +1,5 @@
+import { VxeTableDefines } from '../table'
+
 /* eslint-disable no-use-before-define */
 
 export interface TableMenuMethods<D = any> {
@@ -10,10 +12,10 @@ export interface TableMenuMethods<D = any> {
 export interface TableMenuPrivateMethods<D = any> {
   moveCtxMenu(evnt: KeyboardEvent, ctxMenuStore: any, property: 'selectChild' | 'selected', hasOper: boolean, operRest: any, menuList: any[]): void
   handleOpenMenuEvent(evnt: Event, type: 'header' | 'body' | 'footer', params: any): void
-  handleGlobalContextmenuEvent(evnt: any): void
-  ctxMenuMouseoverEvent(evnt: any, item: any, child?: any): void
-  ctxMenuMouseoutEvent(evnt: any, item: any): void
-  ctxMenuLinkEvent(evnt: any, menu: any): void
+  handleGlobalContextmenuEvent(evnt: Event): void
+  ctxMenuMouseoverEvent(evnt: Event, item: VxeTableDefines.MenuFirstOption | VxeTableDefines.MenuChildOption, child?: any): void
+  ctxMenuMouseoutEvent(evnt: Event, item: VxeTableDefines.MenuFirstOption | VxeTableDefines.MenuChildOption): void
+  ctxMenuLinkEvent(evnt: Event, menu: VxeTableDefines.MenuFirstOption | VxeTableDefines.MenuChildOption): void
 }
 
 declare module '../grid' {
