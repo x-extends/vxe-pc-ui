0a994c58a27d2625282797d224d91cef2bc8314f
xuliangzhan <xu_liangzhan@163.com>
2026-09-11 21:59:18 +0800

    releases 4.18.11

--- examples/views/button/ButtonTest.vue
+++ examples/views/button/ButtonTest.vue
@@ -229,6 +229,23 @@
       <vxe-button content="自定义图标颜色" icon="fa fa-graduation-cap my-green"></vxe-button>
     </p>
 
+    <div>
+      <vxe-button content="默认颜色" prefix-icon="vxe-icon-add"></vxe-button>
+      <vxe-button status="primary" content="主要颜色" prefix-icon="vxe-icon-delete"></vxe-button>
+      <vxe-button status="success" content="成功颜色" prefix-icon="vxe-icon-print"></vxe-button>
+      <vxe-button status="info" content="信息颜色" prefix-icon="vxe-icon-question-circle"></vxe-button>
+      <vxe-button status="warning" content="警告颜色" prefix-icon="vxe-icon-search"></vxe-button>
+      <vxe-button status="error" content="错误颜色" prefix-icon="vxe-icon-save"></vxe-button>
+    </div>
+    <div style="margin-top: 16px;">
+      <vxe-button content="默认颜色" suffix-icon="vxe-icon-add"></vxe-button>
+      <vxe-button status="primary" content="主要颜色" suffix-icon="vxe-icon-delete"></vxe-button>
+      <vxe-button status="success" content="成功颜色" suffix-icon="vxe-icon-print"></vxe-button>
+      <vxe-button status="info" content="信息颜色" suffix-icon="vxe-icon-question-circle"></vxe-button>
+      <vxe-button status="warning" content="警告颜色" suffix-icon="vxe-icon-search"></vxe-button>
+      <vxe-button status="error" content="错误颜色" suffix-icon="vxe-icon-save"></vxe-button>
+    </div>
+
   </div>
 </template>
 
